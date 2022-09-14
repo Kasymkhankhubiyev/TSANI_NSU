b40fcb8d8c978b06da28aeff8d74921873e59c94
Kasymkhan <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2022-09-14 11:33:16 +0700

    Update discriminant.c

--- task1/discriminant.c
+++ task1/discriminant.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <math.h>
 
@@ -6,21 +5,27 @@
 #define False 0
 
 void calc_discriminant(float A, float B, float C, float* Rx, float* Ry, float* Ix, float* Iy){
-    float D = B**2 - 4*A*C;
-    if (D = 0){
-        *Rx =  (-1 * B)/(2 * a);
+    float D = B*B - 4*A*C;
+    printf("Discriminant = %f\n", D);
+    if (D == 0){
+        *Rx =  (-1 * B)/(2 * A);
         *Ry = *Rx;
     }else{
         if (D > 0){
             *Rx = (-1 * B - sqrt(D))/(2 * A);
             *Ry = (-1 * B + sqrt(D))/(2 * A);
         }else{
-            *Rx = (-1 * B)/(2 * a);
+            *Rx = (-1 * B)/(2 * A);
             *Ry = *Rx;
-            *Ix = (-1 * sqrt(-1 * D))/(2 * a);
+            *Ix = (-1 * sqrt(-1 * D))/(2 * A);
             *Iy = -1 * (*Ix);
         }
     }
+}
+
+void peak_coord (float A, float B, float C, float* Mx, float* My){
+    *Mx = (-1 * B)/(2 * A);
+    *My = -1 * (B*B - 4 * A * C)/ (4 * A);
 }
 
 int check_input_A (float A){
@@ -39,8 +44,6 @@ void entrance (float* A, float* B, float* C){
         printf("Аx^2 + Bx + C = 0, А != 0\n");
     
         scanf("%f", A);
-        float D = -1 * A;
-        printf("%d\n", D);
         scanf("%f", B);
         scanf("%f", C);
     
@@ -52,17 +55,43 @@ void entrance (float* A, float* B, float* C){
 double start(){
     //инициализируем переменные
     float A, B, C;
-    floar Rx = 0, Ry = 0, Ix = 0, Iy = 0;
+    float Rx = 0, Ry = 0, Ix = 0, Iy = 0;
+    float Mx, My;
     entrance(&A, &B, &C);
     
+    calc_discriminant(A, B, C, &Rx, &Ry, &Ix, &Iy);
     printf("коэффициенты из функции start\n");
     printf("A = %f \nB = %f \nC = %f\n", A, B, C);
+    
+    if (Ix == 0){
+        printf("Корний уравнений: \n");
+        printf("x1 = %.3f\n", Rx);
+        printf("x2 = %.3f\n", Ry);
+    }else{
+        printf("Корний уравнений: \n");
+        printf("x1 = %.3f %.3fi\n", Rx, Ix);
+        printf("x2 = %.3f %.3fi\n", Ry, Iy);
+    }
+    
+    peak_coord(A, B, C, &Mx, &My);
+    printf("Координаты вершин параболы :\n");
+    printf("X = %.3f\n", Mx);
+    printf("Y = %.3f\n", My);
+    
+    return 0;
 }
 
 
 int main()
 {
-    start();
+    char q;
+    do{
+        start();
+        printf("Чтобы выйти нажмите: q\n");
+        printf("Чтобы повторить: y\n");
+    
+        q = getchar();
+    }while(q != 'q');
 
     return 0;
 }
