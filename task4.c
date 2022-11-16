7b9d25d9a69cbb25ea4f8ff3d6c1783b8df112ad
Kasymkhan <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2022-11-16 19:47:43 +0700

    Add files via upload

---
+++ task4.c
@@ -0,0 +1,74 @@
+//==============================================================================
+//
+// Title:       task4
+// Purpose:     A short description of the application.
+//
+// Created on:  16.11.2022 at 19:04:14 by TC.
+// Copyright:   ANY. All Rights Reserved.
+//
+//==============================================================================
+
+//==============================================================================
+// Include files
+
+#include <ansi_c.h>
+#include <cvirte.h>     
+#include <userint.h>
+#include <tsani.h>
+#include "task4.h"
+#include "toolbox.h"
+
+//==============================================================================
+// Constants
+
+//==============================================================================
+// Types
+
+//==============================================================================
+// Static global variables
+
+static int panelHandle;
+
+//==============================================================================
+// Static functions
+
+//==============================================================================
+// Global variables
+
+//==============================================================================
+// Global functions
+
+/// HIFN The main entry-point function.
+int main (int argc, char *argv[])
+{
+    int error = 0;
+    
+    /* initialize and load resources */
+    nullChk (InitCVIRTE (0, argv, 0));
+    errChk (panelHandle = LoadPanel (0, "task4.uir", PANEL));
+	
+	ni5251Slot(2);
+    
+    /* display the panel and run the user interface */
+    errChk (DisplayPanel (panelHandle));
+    errChk (RunUserInterface ());
+	
+	ni6251Close():
+
+Error:
+    /* clean up */
+    DiscardPanel (panelHandle);
+    return 0;
+}
+
+//==============================================================================
+// UI callback function prototypes
+
+/// HIFN Exit when the user dismisses the panel.
+int CVICALLBACK panelCB (int panel, int event, void *callbackData,
+        int eventData1, int eventData2)
+{
+    if (event == EVENT_CLOSE)
+        QuitUserInterface (0);
+    return 0;
+}
